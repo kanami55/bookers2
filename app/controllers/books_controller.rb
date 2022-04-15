160485d675ced4a5c5a0f4089a7eed81a463274d
kanami55 <summerwave5959@gmail.com>
2022-04-15 04:44:32 +0000

    [Add] ラッシュメッセージ追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,10 @@ class BooksController < ApplicationController
     @new_book = Book.new(book_params)
     @new_book.user_id = current_user.id
     if @new_book.save
+      flash[:notice] = "投稿に成功しました。"
       redirect_to book_path(@new_book)
     else
+      flash[:alret] = "投稿に失敗しました。"
       @books = Book.all
       render "index"
     end
@@ -37,15 +39,18 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.update(book_params)
     if @book.save
+      flash[:notice] = "変更に成功しました。"
       redirect_to book_path(@book)
     else
+      flash[:alert] = "変更に失敗しました。"
       render "edit"
     end
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:alret] = "削除に成功しました。"
     redirect_to books_path
   end
 
