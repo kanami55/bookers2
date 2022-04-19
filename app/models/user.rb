f056c0259a8eea1cf594d5b9ce170ef8f67bd9fe
kanami55 <summerwave5959@gmail.com>
2022-04-19 12:49:57 +0000

    [Add] commentモデル追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   #アソシエーション
   has_many :books, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :comment, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, length: { in: 2..20 }, uniqueness: true
